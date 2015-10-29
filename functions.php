7104360eee847d0903f161c3f16d3564fa9f1166
ironaddict <ironaddict1126@hotmail.com>
2015-10-29 15:39:30 -0500

    New-Starting
    A New starting point with custom functions, header.css and
    archive-listings template for EPL

--- functions.php
+++ functions.php
@@ -48,4 +48,79 @@ require_once( 'library/custom-nav.php' );
 /** Add protocol relative theme assets */
 require_once( 'library/protocol-relative-theme-assets.php' );
 
+/**Change Howdy Message **/
+function howdy_message($translated_text, $text, $domain) {
+    $new_message = str_replace('Howdy', 'Welcome', $text);
+    return $new_message;
+}
+add_filter('gettext', 'howdy_message', 10, 3);
+/** Remove Edit Link */
+add_filter( 'edit_post_link', '__return_false' );
+//*Remove Fields on EPL
+function epl_unset_property_ensuite ($field) {
+return;
+}
+add_filter('epl_meta_property_ensuite', 'epl_unset_property_ensuite');
+function epl_unset_property_rooms ($field) {
+return;
+}
+add_filter('epl_meta_property_rooms', 'epl_unset_property_rooms');
+function epl_unset_property_toilet ($field) {
+return;
+}
+add_filter('epl_meta_property_toilet', 'epl_unset_property_toilet');
+function epl_unset_property_land_fully_fenced ($field) {
+return;
+}
+add_filter('epl_meta_property_land_fully_fenced', 'epl_unset_property_land_fully_fenced');
+function epl_unset_property_air_conditioning ($field) {
+return;
+}
+add_filter('epl_meta_property_air_conditioning', 'epl_unset_property_air_conditioning');
+//*Change Category on EPL
+function epl_modify_property_category($array) {
+    $array = array(
+		'home'      =>   'Single-Family Home',
+		'condo'     =>   'Condo',
+		'townhome'  =>   'Townhome',
+		'mobile'    =>   'Mobile',
+    );
+     
+    return $array;
+}
+add_filter('epl_listing_meta_property_category', 'epl_modify_property_category');
+//* Modifies the edit listing land sizes
+function my_epl_opts_area_unit_filter() {
+    $sizes = array(
+        'sqft'      =>   __('Square Feet', 'epl')
+    );
+    return $sizes;
+}
+// Modifies the edit listing land sizes
+add_action( 'epl_opts_area_unit_filter' , 'my_epl_opts_area_unit_filter');
+ 
+// Modifies the search widget land sizes
+add_action( 'epl_listing_search_land_unit_label' , 'my_epl_opts_area_unit_filter');
+function my_epl_opts_building_area_unit_filter() {
+    $sizes = array(
+		'sqft'      =>   __('Square Feet', 'epl')
+    );
+    return $sizes;
+}
+
+/** Remove Additional Features in Property Entry */
+ function my_epl_unset_additional_features($group) {
+    return;
+}
+add_filter('epl_meta_box_block_epl_additional_features_section_id', 'my_epl_unset_additional_features');
+/** Adjust Easy Property Listings Rental Listings to accept decimals*/
+/* function my_epl_price_number_format() {
+    $decimal = 'decimal';
+    return $decimal;
+}
+add_filter( 'epl_price_number_format' , 'my_epl_price_number_format' , 1 ); */
+
+//* Modify Property (Property)& Rural Listing Type categories
+
+
 ?>
